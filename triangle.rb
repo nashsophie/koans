5a15018ffd261e371b060938716696cf644183f5
sue <p0pxyr@gmail.com>
2013-01-15 03:17:40 +0800

    150/280 done

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,12 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if ((a == b)&(b == c))
+  :equilateral
+  elsif ((a == b) & (b != c)) or ((a!=c) & (b=c))
+  :isoceles
+  else
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
