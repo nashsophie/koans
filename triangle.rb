52a20251ed79ffdca4e791fdf84dd0486c047b96
p0pxyr <p0pxyr@gmail.com>
2013-01-16 02:00:05 +0800

    182/280 done

--- triangle.rb
+++ triangle.rb
@@ -13,15 +13,31 @@
 # and
 #   about_triangle_project_2.rb
 #
-def triangle(a, b, c)
-  # WRITE THIS CODE
-  if ((a == b)&(b == c))
-  :equilateral
-  elsif ((a == b) & (b != c)) or ((a!=c) & (b=c))
-  :isoceles
-  else
-  :scalene
+Tolerance = 0.0001
+
+def is_valid_triangle(a, b ,c)
+    sum_of = (a + b).abs
+      diff_of = (a - b).abs
+        return diff_of < c && c < sum_of
 end
+
+def triangle(a, b, c)
+    raise TriangleError, "sides must be strictly positive" unless a > 0 && b > 0 && c > 0
+      raise TriangleError, "only triangles embeddable in the euclidean plane allowed" unless is_valid_triangle(a,b,c)
+        a_b_are_equals = ( (a-b).abs < Tolerance )
+          a_c_are_equals = ( (a-c).abs < Tolerance )
+            b_c_are_equals = ( (b-c).abs < Tolerance )
+              isosceles = a_b_are_equals || a_c_are_equals || b_c_are_equals
+                equilateral = a_b_are_equals && a_c_are_equals
+                  if equilateral
+                        return :equilateral
+                          end
+                    if isosceles
+                          return :isosceles
+                            end
+                      return :scalene
+end
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
